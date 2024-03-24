2dcf3bed499936163ae23b1c0ea2fb7a87aaafc8
Jacek Miecznikowski <jacekmiecznikowski@outlook.com>
2024-03-24 08:38:28 +0100

    Creating custom user model

--- onlyvans/account/models.py
+++ onlyvans/account/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    is_content_creator = models.BooleanField(default=False, verbose_name="Content creator",
+                                             help_text=_("Designates whether the user is a content creator."))
+    def __str__(self):
+        return self.username
